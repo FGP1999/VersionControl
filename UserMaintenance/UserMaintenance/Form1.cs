0163ab56af25e564a5005c562aca4f5d61749450
Gábor Pál Filep <filepgp@outlook.hu>
2020-09-26 15:35:11 +0200

    Delete Function

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -22,6 +22,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
             button2.Text = Resource1.Write;
+            button3.Text = Resource1.Delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -51,6 +52,12 @@ namespace UserMaintenance
                     sw.WriteLine();
                 }
             }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            User item = (User)listBox1.SelectedItem;
+            users.Remove(item);
         }
     }
 }
