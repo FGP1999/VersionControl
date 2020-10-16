026f740f017c5bfb922771105dab0436a03c46dd
Gábor Pál Filep <filepgp@outlook.hu>
2020-10-16 18:05:40 +0200

    Webszolgaltatas_Elso_resz

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.RateData> Rates = new BindingList<Entities.RateData>();
         public Form1()
         {
             InitializeComponent();
+            DataGridView dgw = new DataGridView();
+            dgw.DataSource = Rates;
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
         }
     }
 }
