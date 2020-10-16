a218d32ca5b35f6cee85d6636b20836e9b2843b8
Gábor Pál Filep <filepgp@outlook.hu>
2020-10-16 19:21:33 +0200

    Webszolgaltatas_Feladat5

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
@@ -14,14 +15,17 @@ namespace Webszolgaltatas
     public partial class Form1 : Form
     {
         BindingList<Entities.RateData> Rates = new BindingList<Entities.RateData>();
+        public string pst;
         public Form1()
         {
             InitializeComponent();
             DataGridView dgw = new DataGridView();
             dgw.DataSource = Rates;
+            GetExchangeRatesFunction();
+            XMLProcess();
         }
 
-        private void GetExchangeRates()
+        private void GetExchangeRatesFunction()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -35,7 +39,29 @@ namespace Webszolgaltatas
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+            pst = result;
+        }
 
+        private void XMLProcess()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(pst);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new Entities.RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
